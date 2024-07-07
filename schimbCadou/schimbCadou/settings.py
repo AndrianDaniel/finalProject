8ab5dfdc83ab95205b683c19862875a6ee51706f
AndrianDaniel <andrian.dorin.daniel@gmail.com>
2024-07-07 14:45:45 +0300

    created app home

--- schimbCadou/schimbCadou/settings.py
+++ schimbCadou/schimbCadou/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Bucharest'
 
 USE_I18N = True
 
